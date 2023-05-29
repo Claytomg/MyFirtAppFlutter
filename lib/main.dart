286a45b8baa84240f2e75f3c801c1e606454718e
Claytomg <claytomguilherme@gmail.com>
2023-05-29 19:08:36 -0400

    to after FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Incremento',
-        child: const Icon(Icons.add_alert),
+        child: const Icon(Icons.add),
       ),
     );
   }
